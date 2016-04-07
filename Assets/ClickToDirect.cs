d7e48dbcf4734e59d166d42b5390b84a577598eb
B_line <blake.hobbs@my.jcu.au>
2016-04-07 18:39:39 +1000

    Started a test level
    hehehehehe

---
+++ Assets/ClickToDirect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToDirect : MonoBehaviour
+{
+	public NavMeshAgent[] controlledAgents;
+	
+	void Update()
+	{
+		//Check if the fire button is being held down
+		if (Input.GetButton("Fire1"))
+		{
+			//Perform a raycast to determine where the user clicked
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+	        if (Physics.Raycast(ray, out hit, 100))
+			{
+				//Direct each of our agents to the new target
+				Vector3 targetPos = hit.point;
+				foreach (NavMeshAgent agent in controlledAgents) {
+					agent.destination = targetPos;
+				}
+	        }
+		}
+	}
+}
