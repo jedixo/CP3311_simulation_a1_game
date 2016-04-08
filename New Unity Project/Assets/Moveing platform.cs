2805b6778e5ff351110675e12408cef9a7fb3642
B_line <blake.hobbs@my.jcu.au>
2016-04-08 20:16:58 +1000

    I uploaded the wrong project
    woops

---
+++ New Unity Project/Assets/Moveing platform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveingplatform : MonoBehaviour {
+    public float x;
+    public float y;
+    public float z;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
