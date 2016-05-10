1527df04e91fbc81513b2bec27fca3485a9615ec
jedixo <jake.dixon@my.jcu.edu.au>
2016-05-10 22:17:43 +1000

    added particles and lighting to desert level and lava

---
+++ New Unity Project/Assets/Custom Assets/desertAssets/scripts/playerFollowPlatofrm.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerFollowPlatofrm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.tag == "platform"){
+			transform.parent = other.transform;
+
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "platform"){
+			transform.parent = null;
+
+		}
+	}    
+}
