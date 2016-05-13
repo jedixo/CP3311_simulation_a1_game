4537a965b1c251ab7dfb83f4ff564724c876c792
B_line <blake.hobbs@my.jcu.au>
2016-05-13 18:10:21 +1000

    Changed the Cenote level a bit
    the platform still wont work

--- New Unity Project/Assets/Custom Assets/desertAssets/scripts/movePlatform.cs
+++ New Unity Project/Assets/Custom Assets/desertAssets/scripts/movePlatform.cs
@@ -1,72 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movePlatform : MonoBehaviour {
+public class movePlatform : MonoBehaviour
+{
 
-	public bool x = false;
-	public bool y = false;
-	public bool z = false;
-	public bool limit = true;
-	public double maxValue = 1;
-	public double minValue = 0;
-	public float delta = (float)0.03;
+    public bool x = false;
+    public bool y = false;
+    public bool z = false;
+    public bool limit = true;
+    public double maxValue = 1;
+    public double minValue = 0;
+    public float delta = (float)0.03;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-			transform.Translate (getDelta (x), getDelta(y), getDelta(z));
-			checkLimit (limit);
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	float getDelta (bool axis) {
-		if (axis) {
-			return delta;
-		}
-		return 0f;
-	}
+    }
 
-	void checkLimit(bool lim) {
-		if (lim) {
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(getDelta(x), getDelta(y), getDelta(z));
+        checkLimit(limit);
+    }
 
-			if (x) {
-				if (transform.position.x >= maxValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			} else if (y) {
-				if (transform.position.y >= maxValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			} else {
-				if (transform.position.z >= maxValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			}
-		} else {
-			
-			if (x) {
-				if (transform.position.x <= minValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			} else if (y) {
-				if (transform.position.y <= minValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			} else {
-				if (transform.position.z <= minValue) {
-					delta = delta * -1;
-					limit = !lim;
-				}
-			}
-		}
-	}
+    float getDelta(bool axis)
+    {
+        if (axis)
+        {
+            return delta;
+        }
+        return 0f;
+    }
+
+    void checkLimit(bool lim)
+    {
+        if (lim)
+        {
+
+            if (x)
+            {
+                if (transform.position.x >= maxValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+            else if (y)
+            {
+                if (transform.position.y >= maxValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+            else {
+                if (transform.position.z >= maxValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+        }
+        else {
+
+            if (x)
+            {
+                if (transform.position.x <= minValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+            else if (y)
+            {
+                if (transform.position.y <= minValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+            else {
+                if (transform.position.z <= minValue)
+                {
+                    delta = delta * -1;
+                    limit = !lim;
+                }
+            }
+        }
+    }
 }
