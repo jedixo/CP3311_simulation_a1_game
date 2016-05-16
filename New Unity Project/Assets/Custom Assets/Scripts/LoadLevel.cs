f835a20cacae255eb96d269a0d510228cac4589d
jedixo <jake.dixon@my.jcu.edu.au>
2016-05-16 14:40:30 +1000

    wprking portals

---
+++ New Unity Project/Assets/Custom Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	public int sceneNumber;
+
+	void OnTriggerEnter(Collider other) {
+		Application.LoadLevel(sceneNumber);
+	}
+}
--- New Unity Project/Assets/Custom Assets/desertAssets/scripts/addGem.cs
+++ New Unity Project/Assets/Custom Assets/desertAssets/scripts/addGem.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class addGem : MonoBehaviour {
 
 	public GameObject levelVariables;
+	public GameObject portalPane;
 
 	// Use this for initialization
 	void Start () {
 		levelVariables = GameObject.Find("levelVariables");
+		portalPane = GameObject.FindGameObjectWithTag("portalPane");
+		portalPane.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -17,7 +20,9 @@ public class addGem : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
 		levelVariables.GetComponent<currencyRecorder> ().gems++;
+		portalPane.SetActive (true);
 		Destroy (this.gameObject);
+
 
 	}
 }
