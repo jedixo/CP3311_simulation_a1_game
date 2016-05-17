efd2fd228e76029e8ac4549023c8a6dd23d7e0b6
B_line <blake.hobbs@my.jcu.au>
2016-05-17 16:22:00 +1000

    Character Controller and Mouse Look Working
    Need to replace model
    setting need to be configured
    thoughts on camera?

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float rotateSpeed = 3;
+    public float fowardSpeed = 10;
+    public float jumpHieght= 13;
+    private CharacterController playerController;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                playerController.Move(new Vector3(0, jumpHieght, 0));
+            }
+        }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float speed = fowardSpeed * Input.GetAxis("Vertical");
+        playerController.SimpleMove(speed * forward);
+    }
+
+}
