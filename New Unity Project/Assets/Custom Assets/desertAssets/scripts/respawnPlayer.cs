b45ea720fe6a75fb692ceb661db8a56a0753733e
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-05-26 23:17:37 +1000

    added coins and respawn fix

--- New Unity Project/Assets/Custom Assets/desertAssets/scripts/respawnPlayer.cs
+++ New Unity Project/Assets/Custom Assets/desertAssets/scripts/respawnPlayer.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class respawnPlayer : MonoBehaviour {
 
-	public GameObject player;
-
+	private GameObject player;
+	public float x = 0;
+	public float y = 0;
+	public float z = 0;
+	public float deathHeight = -5;
 	// Use this for initialization
 	void Start () {
+		player = this.gameObject;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (player.transform.position.y <= -5) {
-			Vector3 temp = new Vector3(0,0,0);
+		if (player.transform.position.y <= deathHeight) {
+			Vector3 temp = new Vector3(x,y,z);
 			player.transform.position = temp;
 		}
 	
